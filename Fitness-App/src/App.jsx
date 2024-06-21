b57ace1cc9565000686b7c20681c939a0d3eaa7d
Grayson Gooden <gygooden@gmail.com>
2024-06-20 18:23:22 -0700

    Begin routing and implement LandingPage

--- Fitness-App/src/App.jsx
+++ Fitness-App/src/App.jsx
@@ -1,15 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from "./Header.jsx";
 import Footer from "./Components/Pages/WorkoutInput/Footer.jsx";
 import Workout from "./Components/Pages/WorkoutInput/Workout.jsx";
 import SetCard from "./Components/Pages/WorkoutInput/SetCard.jsx";
+import LandingPage from './Pages/LandingPage/LandingPage';
 
 function App() {
   return (
-    <>
-      <Header></Header>
-      <SetCard></SetCard>
-      <Footer></Footer>
-    </>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/workout" component={Workout} />
+        <Route path="/set-card" component={SetCard} />
+        /* Add more routes as needed */
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
