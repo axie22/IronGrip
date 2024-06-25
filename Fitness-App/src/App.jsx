c0f16a4835221faee7db330e3eaf4925a23abf41
axie22 <alexxie9667@gmail.com>
2024-06-24 21:42:09 -0400

    File Placement changed

--- Fitness-App/src/App.jsx
+++ Fitness-App/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./Header.jsx";
-import Footer from "./Components/Footer.jsx";
+import Footer from "./Footer.jsx";
 import Workout from "./Components/Pages/WorkoutInput/Workout.jsx";
 import SetCard from "./Components/Pages/WorkoutInput/SetCard.jsx";
 import LandingPage from './Components/Pages/LandingPage/LandingPage.jsx';
