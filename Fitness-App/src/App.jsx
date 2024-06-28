8172148d91ba452ec0c9330c43d6b4dc7734ea76
Grayson Gooden <gygooden@gmail.com>
2024-06-27 21:34:13 -0700

    Footer sizing

--- Fitness-App/src/App.jsx
+++ Fitness-App/src/App.jsx
@@ -3,20 +3,18 @@ import { Outlet } from 'react-router-dom';
 import Footer from "./Footer.jsx";
 import Navbar from "./Navbar.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
   return (
-    <>
+    <div id="root">
       <Navbar />
-      <Outlet />
+      <div className="content">
+        <Outlet />
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
-  /* 
-    Later on we're going to want to add id attributes to the paths so they aren't hardcoded
-    <Route path="/myprofile/:id" element={<Workout />} />
-
-  */
 
 export default App;
