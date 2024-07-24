6c7a1b56a0154688c7fd3899d22f3eb12825258e
Grayson Gooden <gygooden@gmail.com>
2024-07-24 04:11:26 -0700

    Changes from routing vid

--- Fitness-App/src/Backend/routes/users.js
+++ Fitness-App/src/Backend/routes/users.js
@@ -1,3 +1,31 @@
+const router = require("express").Router();
+const { User, validate } = require("../models/user");
+const bcrypt = require("bcrypt");
+
+router.post("/", async (req, res) => {
+	try {
+		const { error } = validate(req.body);
+		if (error)
+			return res.status(400).send({ message: error.details[0].message });
+
+		const user = await User.findOne({ email: req.body.email });
+		if (user)
+			return res
+				.status(409)
+				.send({ message: "User with given email already Exist!" });
+
+		const salt = await bcrypt.genSalt(Number(process.env.SALT));
+		const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+		await new User({ ...req.body, password: hashPassword }).save();
+		res.status(201).send({ message: "User created successfully" });
+	} catch (error) {
+		res.status(500).send({ message: "Internal Server Error" });
+	}
+});
+
+module.exports = router;
+/*
 import express from 'express';
 const router = express.Router();
 
@@ -23,7 +51,6 @@ router
         res.send(`Delete user with id: ${req.params.id}`)
     })
 
-/* 
 router.get('/:id', (req, res) => {
     res.send(`Get user with id: ${req.params.id}`)
 })
@@ -37,7 +64,6 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
     res.send(`Delete user with id: ${req.params.id}`)
 })
-*/
-
 
 export default router
+*/
